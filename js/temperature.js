2cdc2da9cbb7a30eef0b1e3648c424b07d09b914
Afton Downey and Michael Lor <student@epicodus.com>
2016-03-15 16:34:32 -0700

    add math.floor to functions

--- js/temperature.js
+++ js/temperature.js
@@ -1,9 +1,9 @@
 exports.converterC = function(temperature) {
   var celsius = temperature - 273.15;
-  return celsius;
+  return Math.floor(celsius);
 }
 
 exports.converterF = function(temperature) {
   var farenheit = 1.8*(temperature - 273) + 32;
-  return farenheit;
+  return Math.floor(farenheit);
 }
